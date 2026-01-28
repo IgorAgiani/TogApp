078183f1f6a6e4d1316367146684bd58441cde0a
Cristiane <criishernandes1101@gmail.com>
2026-01-27 23:34:48 -0300

    123

---
+++ app/src/main/java/com/example/togapp/WebViewActivity.java
@@ -0,0 +1,32 @@
+package com.example.togapp;
+
+import static android.content.Intent.getIntent;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        WebView webView = findViewById(R.id.meuWebView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        String url = getIntent().getStringExtra("url_destino");
+
+        if (url != null) {
+            webView.loadUrl(url);
+        }
+    }
+}
